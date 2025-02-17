7412317e4d9f1abaac9970151762a065dac0b315
zhaoyf <604264588@qq.com>
2025-02-17 18:16:22 +0800

    feat: add google analytics

---
+++ src/utils/analytics.ts
@@ -0,0 +1,16 @@
+// @ts-nocheck
+export const analytics = () => {
+    window.dataLayer = window.dataLayer || []
+    window.gtag = function gtag() {
+      window.dataLayer.push(arguments) // eslint-disable-line
+    }
+    window.gtag("js", new Date())
+    window.gtag("config", process.env.PLASMO_PUBLIC_GTAG_ID, {
+      page_path: "/popup",
+      debug_mode: true
+    })
+
+    window.gtag("event", "login", {
+      method: "TEST"
+    })
+}
