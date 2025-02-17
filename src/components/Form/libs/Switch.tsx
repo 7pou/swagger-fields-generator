3d4f3cf966df2659e8b3a910d8466820e7fb02d9
zhaoyf <604264588@qq.com>
2025-02-18 00:15:29 +0800

    feat: add Size props

--- src/components/Form/libs/Switch.tsx
+++ src/components/Form/libs/Switch.tsx
@@ -1,21 +1,19 @@
-interface Props {
-    value?: boolean
-    onChange?: (value: boolean) => void
-}
 import React from 'react';
 
 import "../index.css"; // 引入 CSS 文件
 
 interface SwitchProps {
     value?: boolean;
+    size?: "small" | "default";
     onChange?: (value: boolean) => void;
 }
 
-const Switch: React.FC<SwitchProps> = ({ value, onChange }) => {
+const Switch: React.FC<SwitchProps> = ({ value, onChange, size }) => {
   return (
     <span
       onClick={() => onChange(!value)}
-      className={`swagger-fields-generator_switch ${value ? " on" : ""}`}
+      className={`swagger-fields-generator_switch ${value ? " on" : ""} ${size ? " " + size : ""}
+      }`}
     >
       <div className="switch-handle" />
     </span>
