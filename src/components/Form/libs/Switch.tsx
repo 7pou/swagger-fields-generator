d18b230edf5dc0d1d3687d74130740e646a63cb9
zhaoyf <604264588@qq.com>
2025-02-20 10:36:17 +0800

    feat: add if props

--- src/components/Form/libs/Switch.tsx
+++ src/components/Form/libs/Switch.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import "../index.css"; // 引入 CSS 文件
 
 interface SwitchProps {
+    if?: boolean;
     value?: boolean;
     size?: "small" | "default";
     onChange?: (value: boolean) => void;
 }
 
-const Switch: React.FC<SwitchProps> = ({ value, onChange, size }) => {
+const Switch: React.FC<SwitchProps> = ({ if:condition = true,value, onChange, size }) => {
+  if (condition !== true) return null;
   return (
     <span
       onClick={() => onChange(!value)}
