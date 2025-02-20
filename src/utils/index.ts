7872125d3af3c0f53cb011127ce3532f51e956a7
zhaoyf <604264588@qq.com>
2025-02-20 20:10:56 +0800

    feat: add downloadJson

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -66,3 +66,16 @@ export const deepClone = (data, cache = new WeakMap()) => {
   }
   return cloneTarget
 }
+
+
+export const downloadJson = (jsonData) => {
+  const blob = new Blob([JSON.stringify(jsonData, null, 2)], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'swagger-fields-config.json'; 
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a); 
+  URL.revokeObjectURL(url);
+}
