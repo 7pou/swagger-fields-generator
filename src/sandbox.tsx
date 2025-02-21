bcf4fad0ef3d770bd257eb2e78af1c8a1977970a
zhaoyf <604264588@qq.com>
2025-02-22 00:23:02 +0800

    fixx: 修改transferOpenapi

--- src/sandbox.tsx
+++ src/sandbox.tsx
@@ -1,61 +1,72 @@
 import { useEffect, useState } from "react"
 import { MessageType } from "~common/messageType"
 import { deepClone } from "~utils"
-const requestParamsformat = (data) => {}
-const requestBodyformat = (data) => {}
-const responsesformat = (data) => {}
+const requestParamsformat = (api, model) => {
+  const params = api.parameters?.filter((p) => p.in === 'query')
+  if (!params) return null
+  const schema = findSchema(params.schema, model)
+  return schema
+}
+const requestBodyformat = (api, model) => {
+  const body = api.parameters?.find((p) => p.in === 'body')
+  if (!body) return null
+  const schema = findSchema(body.schema, model)
+  return schema
+}
+const responsesformat = (data,model) => {
+  const res = data?.responses?.['200']
+  if (!res) return null
+  const schema = findSchema(res.schema, model)
+  return schema
+}
 
+const findSchema = (obj, definitions) => {
+  if (!obj) return obj
+  if (obj && obj.$ref) {
+    let copyDefinitions = deepClone(definitions)
+    const refPaths = obj.$ref.split('/')
+    for (let i = 1; i < refPaths.length; i++) {
+      const path = refPaths[i];
+      copyDefinitions = copyDefinitions[path]
+    }
+    obj = findSchema(copyDefinitions, definitions)
+  }
+  if (obj.type === 'array') {
+    obj.items = findSchema(obj.items, definitions)
+  }
+
+  if (obj.type === 'object') {
+    const properties = obj.properties
+    const result = {}
+    for (let key in properties) {
+      result[key] = findSchema(properties[key], definitions)
+    }
+    obj.properties = result
+  }
+  return obj
+}
 
 /**
  * 抹平版本字段差异
- * @param jsonData 
+ * @param jsonData
  */
 
-const data = (req) => {
+const transferOpenapi = (api, json) => {
   const item: any = {}
-  item.method = req.method;
+  item.path = api.path;
+  item.method = api.method;
+  item.tags = api.tags?.[0]
+  item.summary = api.summary
+  item.description = api.description
+  item.operationId = api.operationId
+  item.consumes = api.consumes
+  item.produces = api.produces
 
-  item.operationId = req.operationId
-  item.summary = req.summary
-  item.tags = req.tags?.[0]
-  item.description = req.description
-  item.consumes = req.consumes
-  item.produces = req.produces
-  item.requestParams = requestParamsformat(req)
-  item.requestBody = requestBodyformat(req)
-  item.responsesData = responsesformat(req)
-}
+  item.requestParams = requestParamsformat(api, json)
+  item.requestBody = requestBodyformat(api, json)
+  item.responsesData = responsesformat(api, json)
 
-export const jsonformat = (jsonData) => {
-    const paths = Object.keys(jsonData.paths)
-    const result = []
-    function findSchema(ref, schema) {
-        if (!ref) return null
-        const parts = ref.split('/').slice(1); // 移除 `#`
-        let result = deepClone(schema);
-        
-        for (const part of parts) {
-            if (result && result[part]) {
-                result = result[part];
-            } else {
-                return null; // 未找到
-            }
-        }
-        return result;
-    }
-
-    for (let i = 0; i < paths.length; i++) {
-        const item: any = {}
-        item.path = paths[i];
-        const methodObj = jsonData.paths[item.path]
-        const methods = Object.keys(methodObj)
-
-        for (let i = 0; i < methods.length; i++) {
-            
-        }
-        result.push(item)
-    }
-    return result
+  return item
 }
 
 const IndexSandbox = () => {
@@ -66,8 +77,9 @@ const IndexSandbox = () => {
       if (event.data.type === MessageType.EXECUTE) {
         try {
           const userFunction = new Function("return " + event.data.code)();
-          // const a = jsonformat(event.data.input);
-          const result = userFunction(event.data.input)
+          const openapi = transferOpenapi(event.data.input, event.data.json);
+
+          const result = userFunction(openapi)
           const code = typeof result === 'string' ? result : JSON.stringify(result, null, 2);
           setCode(code);
           setStatus('success');
