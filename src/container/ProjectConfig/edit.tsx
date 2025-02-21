b0a9b449d20d4cf8546cbc18431f22469db570a9
zhaoyf <604264588@qq.com>
2025-02-21 22:26:33 +0800

    feat: 添加按钮排序

--- src/container/ProjectConfig/edit.tsx
+++ src/container/ProjectConfig/edit.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useImperativeHandle, useRef, useState } from "react"
+import { forwardRef, useImperativeHandle, useMemo, useRef, useState } from "react"
 import Button from "~components/Button"
 import Flex from "~components/Flex"
 import FormItem from "~components/Form/Item"
@@ -7,23 +7,24 @@ import Textarea from "~components/Form/libs/Textarea"
 import Modal from "~components/Modal"
 import Space from "~components/Space"
 import type { ProjectConfigProps } from "~storage/project"
-import { stringSplit } from "~utils"
+import { deepClone, stringSplit } from "~utils"
 
 const ProjectConfigEdit = forwardRef((props: any, ref) => {
     const [visible, setVisible] = useState(false)
     const [data, setData] = useState<ProjectConfigProps>({})
     const handleResolve: any = useRef();
+    const dragRef: any = useRef()
     const handleChange = (key: keyof ProjectConfigProps, value: any) => {
         setData({ ...data,[key]: value})
     }
-     
+
     const open = (data?: ProjectConfigProps) => {
         const defaultData = {
         }
         setVisible(true)
 
         setData(data || defaultData)
-        
+
         return new Promise<void>((resolve, reject) => {
             handleResolve.current = resolve
         })
@@ -55,26 +56,65 @@ const ProjectConfigEdit = forwardRef((props: any, ref) => {
         }
         return [...list, btn].join(',')
     }
+    const handleDragStart = (e,uuid) => {
+      dragRef.current = {
+        target: e.target,
+        uuid
+      }
+    }
+    const handleDragEnter = (e, uuid) => {
+        if (dragRef.current.target === e.target) return
+        if (!stringSplit(data.btns).includes(uuid)) return
+        setData(data => {
+          const dragIndex = stringSplit(data.btns).findIndex(item => item == dragRef.current.uuid)
+          const dropIndex = stringSplit(data.btns).findIndex(item => item == uuid)
+          const btns = [...stringSplit(data.btns)]
+          btns.splice(dragIndex, 1)
+          btns.splice(dropIndex, 0, dragRef.current.uuid)
+          return {...data, btns: btns.join(',')}
+        })
+
+    }
+    const viewerList = useMemo(() => {
+      const generates = deepClone(props.generate)
+      const result = []
+       stringSplit(data.btns).forEach(item => {
+        const index = generates.findIndex(e => e.uuid === item)
+        if ( index === -1) return
+        result.push(generates.splice(index, 1)[0])
+      })
+      result.push(...generates)
+      return result
+    },[data.btns, props.generate])
     return (
 
         <Modal title={chrome.i18n.getMessage(data.uuid ? 'edit': 'create')} open={visible} footer={renderFooter()}>
             <FormItem width={100} label="URL">
               <Input value={data.url} onChange={(e) => handleChange('url', e)}  />
             </FormItem>
-          
+
             <FormItem width={100} label="json src">
               <Textarea value={data.json} onChange={e => handleChange('json', e)} />
             </FormItem>
             <FormItem width={100} label={chrome.i18n.getMessage('button_name')}>
                 <Space>
-                    {props.generate?.map(item => (
+                    {viewerList?.map(item => (
                         stringSplit(data.btns).includes(item.uuid) ?
-                        <Button size="small" type="ghost" key={item.uuid} onClick={() => handleChange('btns', actionBtns(item.uuid))}>{item.btnName}</Button>:
+                        <Button
+                          draggable
+                          onDragStart={(e) => handleDragStart(e, item.uuid)}
+                          onDragEnter={(e) => handleDragEnter(e, item.uuid)}
+                          onDragOver={e => e.preventDefault()}
+                          size="small"
+                          type="ghost"
+                          key={item.uuid}
+                          onClick={() => handleChange('btns', actionBtns(item.uuid))}
+                        >{item.btnName}</Button>:
                         <Button size="small" type="ghost" style={{opacity: 0.5}} key={item.uuid} onClick={() => handleChange('btns', actionBtns(item.uuid))}>{item.btnName}</Button>
                     ))}
                 </Space>
             </FormItem>
-          
+
         </Modal>
     )
 })
