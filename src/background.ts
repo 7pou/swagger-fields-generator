cdc29866f2fa440bab630dbbebabb0f7b77b242c
zhaoyf <604264588@qq.com>
2025-02-22 00:18:14 +0800

    feat: add MessageType.OPTIONS_PARAMS listen

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,11 @@
+import { MessageType } from "~common/messageType";
+
 export {}
 console.log("HELLO WORLD FROM BGSCRIPTS")
+chrome.runtime.onMessage.addListener((message: any) => {
+        console.log(message);
+
+  if (message?.type === MessageType.OPTIONS_PARAMS) {
+    // setTab(tabmap[message.data.tab])
+  }
+})
