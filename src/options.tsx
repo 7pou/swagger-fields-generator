644dfc3ec3a758440523abc2a675a58fbe9649ed
zhaoyf <604264588@qq.com>
2025-02-22 00:17:47 +0800

    feat: 添加 监听tab参数 自动切换 至tab

--- src/options.tsx
+++ src/options.tsx
@@ -9,10 +9,30 @@ import 'https://www.googletagmanager.com/gtag/js?id=$PLASMO_PUBLIC_GTAG_ID'
 import Help from "~container/Help"
 import ErrorBoundary from "~components/ErrorBoundary"
 import ErrorWarning from "~container/ErrorWarning"
+import { getOptionsPageParams } from "~storage/app"
+import { MessageType } from "~common/messageType"
 function IndexOptions() {
   const [tab, setTab] = useState(0)
+  const tabmap = {
+    'global': 0,
+    'generate': 1,
+    'project': 2,
+    'help': 3,
+  }
+
   useEffect(() => {
     analytics()
+    getOptionsPageParams().then((params: any) => {
+      if (params?.tab && tabmap[params.tab] !== undefined) {
+        setTab(tabmap[params.tab])
+      }
+    })
+    chrome.runtime.onMessage.addListener((message: any) => {
+      if (message?.type === MessageType.OPTIONS_PARAMS) {
+        setTab(tabmap[message.params.tab])
+      }
+    })
+
   }, [])
 
   return (
@@ -22,19 +42,19 @@ function IndexOptions() {
           <div className="menus">
             <div className={"menu-item " + (tab === 0 ? ' on' : '')} onClick={() => setTab(0)}>
               <i className="iconfont icon-shezhi"></i>
-              <span>{chrome.i18n.getMessage('global_page')}</span>  
+              <span>{chrome.i18n.getMessage('global_page')}</span>
             </div>
             <div className={"menu-item " + (tab === 1 ? ' on' : '')} onClick={() => setTab(1)}>
               <i className="iconfont icon-AIGC"></i>
-              <span>{chrome.i18n.getMessage('generate_page')}</span>  
+              <span>{chrome.i18n.getMessage('generate_page')}</span>
             </div>
             <div className={"menu-item " + (tab === 2 ? ' on' : '')} onClick={() => setTab(2)}>
               <i className="iconfont icon-project"></i>
-              <span>{chrome.i18n.getMessage('project_page')}</span>  
+              <span>{chrome.i18n.getMessage('project_page')}</span>
             </div>
             <div className={"menu-item " + (tab === 3 ? ' on' : '')} onClick={() => setTab(3)}>
               <i className="iconfont icon-help"></i>
-              <span>{chrome.i18n.getMessage('help')}</span>  
+              <span>{chrome.i18n.getMessage('help')}</span>
             </div>
           </div>
           <div className="content">
@@ -47,7 +67,7 @@ function IndexOptions() {
           </div>
         </div>
         <div className="footer"></div>
-    
+
       </div>
   )
 }
