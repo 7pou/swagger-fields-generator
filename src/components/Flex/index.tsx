2577e84eabd9a5e6272dedfd07e4a3156d62ef30
zhaoyf <604264588@qq.com>
2025-02-22 16:08:43 +0800

    feat: add if props

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -1,18 +1,21 @@
 import type { CSSProperties, ReactNode } from "react"
 
-export interface FlexProps {
+export interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
     className?: string
     style?: CSSProperties
     children?: ReactNode
+    if?: boolean
     justify?: "center" | "flex-start" | "flex-end" | "space-between" | "space-around" | "space-evenly"
     align?: "center" | "flex-start" | "flex-end" | "stretch" | "baseline"
 }
 const Flex = (props: FlexProps) => {
+  const {if: condition = true , style, ...restProps} = props
     const styles: CSSProperties = {
         display: "flex",
         justifyContent: props.justify,
         alignItems: props.align
     }
-    return <div style={{...styles, ...props.style}} className={props.className}>{props.children}</div>
+    if (!condition) return null
+    return <div {...restProps} style={{...styles, ...style}}>{props.children}</div>
 }
 export default Flex
