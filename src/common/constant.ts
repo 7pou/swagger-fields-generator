60d2dee92dd4e9912e1c99026d58d1ee3290c36b
zhaoyf <604264588@qq.com>
2025-02-25 21:22:43 +0800

    feat: add GITHUB_URL

--- src/common/constant.ts
+++ src/common/constant.ts
@@ -1,2 +1,3 @@
 export const T_INVITE_URL = "https://t.me/+EDQY1U7dn401YzFh"
 export const DISCORD_INVITE_URL = "https://discord.gg/mvTPqXKat5"
+export const GITHUB_URL = "https://github.com/7pou/swagger-fields-generator"
