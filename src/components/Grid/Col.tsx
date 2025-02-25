9b9de9ebc58ebb99498973f811c8d80469db550f
zhaoyf <604264588@qq.com>
2025-02-25 21:22:33 +0800

    feat: add width props

--- src/components/Grid/Col.tsx
+++ src/components/Grid/Col.tsx
@@ -1,15 +1,16 @@
 import type { ReactNode } from "react"
 
 interface Props {
-    span?: number  
+    span?: number
+    width?: number
     offset?: number
-    children?: ReactNode 
+    children?: ReactNode
 }
 const span = 24
 const Col = (props: Props) => {
     const span2ratio = (v) => `${v / span * 100}%`
     const styles = {
-        width: span2ratio(props.span || 24),
+        width: props.width || span2ratio(props.span || 24),
         marginLeft: span2ratio(props.offset),
     }
     return (
