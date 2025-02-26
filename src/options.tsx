1b0dd75d052b3f2991814f9936c2f13afdb7b0a5
zhaoyf <604264588@qq.com>
2025-02-26 23:35:53 +0800

    fix: 移除 googletagmanager脚本

--- src/options.tsx
+++ src/options.tsx
@@ -4,7 +4,6 @@ import ProjectConfig from "~container/ProjectConfig"
 import GeneratorConfig from "~container/GeneratorConfig"
 import './styles/options.scss'
 import './styles/iconfont.css'
-import 'https://www.googletagmanager.com/gtag/js?id=$PLASMO_PUBLIC_GTAG_ID'
 import Help from "~container/Help"
 import ErrorBoundary from "~components/ErrorBoundary"
 import ErrorWarning from "~container/ErrorWarning"
