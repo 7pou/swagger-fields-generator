b997068c5f783d4e64568240b5fed316300ec82b
zhaoyf <604264588@qq.com>
2025-03-19 00:11:08 +0800

    fix: 兼容 openapi数据格式

--- src/sandbox.tsx
+++ src/sandbox.tsx
@@ -1,24 +1,6 @@
 import { useEffect, useState } from "react"
 import { MessageType } from "~common/messageType"
-import { compileJsonSchemaToTs, deepClone } from "~utils"
-const requestParamsformat = (api, model) => {
-  const params = api.parameters?.filter((p) => p.in === 'query')
-  if (!params || params.length === 0) return null
-  const schema = findSchema(params, model)
-  return schema
-}
-const requestBodyformat = (api, model) => {
-  const body = api.parameters?.find((p) => p.in === 'body')
-  if (!body) return null
-  const schema = findSchema(body.schema, model)
-  return schema
-}
-const responsesformat = (data,model) => {
-  const res = data?.responses?.['200']
-  if (!res) return null
-  const schema = findSchema(res.schema, model)
-  return schema
-}
+import { compileJsonSchemaToTs, deepClone, formatType } from "~utils"
 
 const findSchema = (obj, definitions) => {
   if (!obj) return obj
@@ -45,6 +27,72 @@ const findSchema = (obj, definitions) => {
   }
   return obj
 }
+const requestParamsformat = (api, model) => {
+  if (model.swagger) {
+    const params = api.parameters?.filter((p) => p.in === 'query')
+    if (!params || params.length === 0) return null
+    const schema = findSchema(params, model)
+    return schema
+  }
+  if (model.openapi) {
+    const params = api.parameters
+    if (!params || params.length === 0) return null
+    const schema = findSchema(params, model)
+    return schema
+  }
+  return null
+}
+const requestBodyformat = (api, model) => {
+  if (model.swagger) {
+    const body = api.parameters?.find((p) => p.in === 'body')
+    if (!body) return null
+    const schema = findSchema(body.schema, model)
+    return schema
+  }
+  if (model.openapi) {
+    const body = api.requestBody
+    if (!body) return null
+    const jsonschema = findSchema(body.content?.['application/json']?.schema, model)
+    const xmlschema = findSchema(body.content?.['application/xml']?.schema, model)
+    const textschema = findSchema(body.content?.['text/plain']?.schema, model)
+    const htmlschema = findSchema(body.content?.['text/html']?.schema, model)
+    const binaryschema = findSchema(body.content?.['application/octet-stream']?.schema, model)
+    const multipartschema = findSchema(body.content?.['multipart/form-data']?.schema, model)
+    const formdataschema = findSchema(body.content?.['application/x-www-form-urlencoded']?.schema, model)
+    const schema = jsonschema || xmlschema || textschema || htmlschema || binaryschema || multipartschema || formdataschema
+    if (schema) {
+      schema.origin = body
+    }
+    return schema
+  }
+  return null
+}
+const responsesformat = (data,model) => {
+  const res = data?.responses?.['200']
+  if (!res) return null
+  if (model.swagger) {
+    return findSchema(res.schema, model)
+  }
+  if (model.openapi) {
+    let schema  = null
+    const jsonschema = res.content?.['application/json']?.schema
+    const xmlschema = res.content?.['application/xml']?.schema
+    const textschema = res.content?.['text/plain']?.schema
+    const htmlschema = res.content?.['text/html']?.schema
+    const binaryschema = res.content?.['application/octet-stream']?.schema
+    const multipartschema = res.content?.['multipart/form-data']?.schema
+    const formdataschema = res.content?.['application/x-www-form-urlencoded']?.schema
+    schema = jsonschema || xmlschema || textschema || htmlschema || binaryschema || multipartschema || formdataschema
+    // 如果只有一个属性，并且没有type，则判断类型
+    if (Object.keys(schema).length === 1 && !schema.type) {
+      schema.type =  formatType(schema[Object.keys(schema)[0]])
+    }
+    return findSchema(schema, model)
+  }
+  return null
+}
+
+
 
 /**
  * 抹平版本字段差异
