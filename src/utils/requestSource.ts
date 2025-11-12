22caeecc492d3a12f0112b80b83b053561a89290
Draven.Zhao <draven.zhao@digit-buzz-apac.com>
2025-11-12 16:57:19 +0800

    fix: 修改 requestSource 不再做拦截逻辑

--- src/utils/requestSource.ts
+++ src/utils/requestSource.ts
@@ -1,25 +1,25 @@
 import { globalConfigStorageGet } from "~storage/global";
 import request from "./request"
 import { projectStorageGetByHost } from "~storage/project";
-import { multiLine2array } from "~utils";
+import { parseTryUrls } from "~utils";
 
-export const requestSource = async (href: string) => {
+export const requestSource = async (origin: string) => {
+
     const config = await globalConfigStorageGet()
     const tryList = []
-    const project = await projectStorageGetByHost(href)
-    if (config.enable === false) return
-    if (!project || project.enable !==  true) return
+    const project = await projectStorageGetByHost(origin)
+
+    if (!project) return
     if (project && project.json) {
-        tryList.push(...multiLine2array(project.json))
+        tryList.push(...parseTryUrls(project.json, origin))
     } else {
-        tryList.push(...multiLine2array(config.json))
+        tryList.push(...parseTryUrls(config.json, origin))
     }
     for (let i = 0; i < tryList.length; i++) {
-        try {
-            return await request({ url: tryList[i] })
-        } catch (error) {
-            
-        }
+      const res = await request({ url: tryList[i] }).catch((err) => {
+        return null
+      })
+      if (res) return await res.json()
     }
     return null
 }
