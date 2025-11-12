bd8873127f0d3c69c69c4d79e60ea63ffcf5add2
Draven.Zhao <draven.zhao@digit-buzz-apac.com>
2025-11-12 17:00:45 +0800

    feat: 添加 loading

--- src/components/Form/libs/Switch.tsx
+++ src/components/Form/libs/Switch.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import '../../../styles/iconfont.css'
 import "../index.css"; // 引入 CSS 文件
 
 interface SwitchProps {
     if?: boolean;
     value?: boolean;
+    loading?: boolean;
     size?: "small" | "default";
     onChange?: (value: boolean) => void;
 }
 
-const Switch: React.FC<SwitchProps> = ({ if:condition = true,value, onChange, size }) => {
+const Switch: React.FC<SwitchProps> = ({ if:condition = true,value, onChange, size, loading }) => {
   if (condition !== true) return null;
   return (
     <span
@@ -17,7 +18,7 @@ const Switch: React.FC<SwitchProps> = ({ if:condition = true,value, onChange, si
       className={`swagger-fields-generator_switch ${value ? " on" : ""} ${size ? " " + size : ""}
       }`}
     >
-      <div className="switch-handle" />
+      <div className={`switch-handle ${loading ? "iconfont icon-loading" : ""}`} />
     </span>
   );
 };
