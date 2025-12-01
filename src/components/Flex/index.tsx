060cc125830e4f28476c7ba352c2ca48ba564d02
Draven.Zhao <draven.zhao@digit-buzz-apac.com>
2025-12-01 14:37:59 +0800

    feat: 添加 gap direction 属性

--- src/components/Flex/index.tsx
+++ src/components/Flex/index.tsx
@@ -5,6 +5,8 @@ export interface FlexProps extends React.HTMLAttributes<HTMLDivElement> {
     style?: CSSProperties
     children?: ReactNode
     if?: boolean
+    gap?: string | number;
+    direction?: 'row' | 'column';
     justify?: "center" | "flex-start" | "flex-end" | "space-between" | "space-around" | "space-evenly"
     align?: "center" | "flex-start" | "flex-end" | "stretch" | "baseline"
 }
@@ -13,7 +15,9 @@ const Flex = (props: FlexProps) => {
     const styles: CSSProperties = {
         display: "flex",
         justifyContent: props.justify,
-        alignItems: props.align
+        alignItems: props.align,
+        flexDirection: props.direction,
+        gap: props.gap
     }
     if (!condition) return null
     return <div {...restProps} style={{...styles, ...style}}>{props.children}</div>
